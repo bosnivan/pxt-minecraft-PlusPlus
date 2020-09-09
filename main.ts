f53eb3350ec31a354dbd6006120b75493238d888
Ivan Bosnić <bosnivan@gmail.com>
2020-09-09 08:49:55 +0200

    soul sand water elevator fixed

--- main.ts
+++ main.ts
@@ -37,19 +37,16 @@ namespace PlusPlus {
     //% blockId="pp_soul_sand_water_elevator" block="soul sand water elevator with height of %height blocks"
 	export function soul_sand_water_elevator(height: number) {
 		let position: Position = positions.groundPosition(player.position())
-		blocks.fill(GLASS, positions.add(position, pos(-1, 0, 4)), positions.add(position, pos(-1, height, 4)), FillOperation.Replace)
-		blocks.fill(GLASS, positions.add(position, pos(0, 0, 5)), positions.add(position, pos(0, height, 5)), FillOperation.Replace)
-		blocks.fill(GLASS, positions.add(position, pos(1, 0, 4)), positions.add(position, pos(1, height, 4)), FillOperation.Replace)
-		blocks.fill(GLASS, positions.add(position, pos(0, 2, 3)), positions.add(position, pos(0, height, 3)), FillOperation.Replace)
-		blocks.fill(GLASS, positions.add(position, pos(-1, height, 3)), positions.add(position, pos(1, height, 5)), FillOperation.Replace)
-		blocks.place(GLASS, positions.add(position, pos(-1, 0, 3)))
-		blocks.place(GLASS, positions.add(position, pos(-1, 1, 3)))
-		blocks.place(GLASS, positions.add(position, pos(1, 0, 3)))
-		blocks.place(GLASS, positions.add(position, pos(1, 1, 3)))
+		blocks.place(SOUL_SAND, positions.add(position, pos(0, -1, 4)))
+		blocks.fill(GLASS, positions.add(position, pos(-1, 0, 3)), positions.add(position, pos(1, 1, 3)), FillOperation.Replace)
 		player.execute("/setblock " + position.getValue(Axis.X) + " " + position.getValue(Axis.Y) + " " + (position.getValue(Axis.Z) + 3) + " wall_sign 5 replace")
 		player.execute("/setblock " + position.getValue(Axis.X) + " " + (position.getValue(Axis.Y) + 1) + " " + (position.getValue(Axis.Z) + 3) + " wall_sign 5 replace")
-		blocks.place(SOUL_SAND, positions.add(position, pos(0, -1, 4)))
-		blocks.place(WATER, positions.add(position, pos(0, height, 4)))
+		blocks.fill(GLASS, positions.add(position, pos(0, 2, 3)), positions.add(position, pos(0, height - 2, 3)), FillOperation.Replace)
+		blocks.fill(GLASS, positions.add(position, pos(-1, 0, 4)), positions.add(position, pos(-1, height - 2, 4)), FillOperation.Replace)
+		blocks.fill(GLASS, positions.add(position, pos(0, 0, 5)), positions.add(position, pos(0, height - 2, 5)), FillOperation.Replace)
+		blocks.fill(GLASS, positions.add(position, pos(1, 0, 4)), positions.add(position, pos(1, height - 2, 4)), FillOperation.Replace)
+		blocks.fill(GLASS, positions.add(position, pos(-1, height - 1, 3)), positions.add(position, pos(1, height - 1, 5)), FillOperation.Replace)
+		blocks.place(WATER, positions.add(position, pos(0, height - 1, 4)))
 	}
 
 }
